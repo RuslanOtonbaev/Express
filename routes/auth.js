2a09b14cd712a004a7c6b18af23aa9fc18c44b9a
rotonbaev <ruslan.otonbaev@zensoft.io>
2019-09-25 14:38:21 +0600

    add joy for validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
 const User = require('../model/user');
-const Joi = require('@hapi/joi');
+const {registerValidation} = require('../validation');
 
-const schema = {
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-};
 
 router.post('/register', async (req, res) => {
 
-    /**const validation = Joi.validate(req.body, schema);
-    res.send(validation); */
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
 
      const user = new User({
         name: req.body.name,
